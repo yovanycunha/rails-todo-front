cf654996493d645e8e5510f798df739630f80b57
Yovany Cunha <yovany33@gmail.com>
2019-08-07 09:26:21 -0300

    full tutorial to guide me

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import './App.scss';
+
+import Task from './components/tasks/Tasks';
+import Header from './components/header/Header';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+library.add(faCheckCircle, faTrashAlt)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Container>
+        <Task/>
+      </Container>
     </div>
   );
 }
