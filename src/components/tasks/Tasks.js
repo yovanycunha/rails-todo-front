58ca44f9513acc715a153df96cd11d5141538df5
Yovany Cunha <yovany33@gmail.com>
2019-08-08 19:28:11 -0300

    some responsivity

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import List from './list/List';
 import CreateTask from './create/CreateTasks';
+import Container from 'react-bootstrap/Container';
 
 const _HEROKU_URL = `https://murmuring-citadel-83821.herokuapp.com/tasks`;
 
@@ -31,15 +32,16 @@ class Tasks extends Component {
 
   render() {
     return (
-      <Row>
-        <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
+
+      <Row >
+        <Col xs={24} md={16} sm={8} className="tasks_list">
           <Row>
             <Col><p className="title">Por Fazer</p></Col>
             <Col><CreateTask loadTasks={this.loadTasks}/></Col>
           </Row>
           <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => task.done !== true)}/>
         </Col>
-        <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
+        <Col xs={24} md={16} sm={8} className="tasks_list">
           <p className="title">Concluídas</p>
           <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => task.done === true)}/>
         </Col>
